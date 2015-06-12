cac6cd7137de6e3021c5edb358df68c13d011b3c
lmeng <lmeng@qiujieda.com>
2015-06-12 15:45:45 +0800

    init commit

---
+++ EasyPR_Android/jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)/src
+include $(CLEAR_VARS)
+# OpenCV
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+include /home/liumeng/share/easypr/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk  
+FILE_LIST := $(wildcard $(LOCAL_PATH)/core/*.cpp)
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_MODULE     := imageproc  
+LOCAL_LDLIBS += -llog 
+include $(BUILD_SHARED_LIBRARY)  
+
